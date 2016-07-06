d9f95ad6676f7884b2b557dfa9d3e1a74278abcc
carlosjlander <carlosj.lander@gmail.com>
2016-07-06 10:41:21 -0400

    Ajustes generales en Home Page

--- tumbao-tours/js/tumbao-tours.js
+++ tumbao-tours/js/tumbao-tours.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
 	$('.carousel').carousel({
         interval: 5000 //changes the speed
     });
+
+	var IniPoint = new Waypoint({
+		element: document.getElementById('carousel-example-generic'),
+		handler:function(){
+			$("#plus1,#plus2,#plus3,#plus4").css('visibility','visible');
+			$("#plus1").addClass('animated slideInLeft');
+			$('#plus1').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', nextAnimate());
+		}
+	});
 });
+
+function nextAnimate(){
+	$("#plus2").addClass('animated fadeInUpBig');
+	$("#plus3").addClass('animated fadeInUpBig');
+	$("#plus4").addClass('animated slideInRight');
+}
